b37f1030eb3e4eb704f80fe20b8553b66e014aa0
Sahuan <sahuanpimentel@gmail.com>
2025-03-01 11:50:06 -0300

    Stylizing the title and the box with breeds contents.

---
+++ src/pages/DogBreedInfo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DogBreedInfo() {
+  return <div>DogBreedInfo</div>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./Home.module.css";
+import DogBreed from "./DogBreed";
 
 export default function Home() {
   return (
-    <div className={styles.header}>
-      <h1>Teste de funcionalidade da página Home!</h1>
+    <div>
+      <div className={styles.header}>
+        <h1>App Raças de Cachorros</h1>
+      </div>
+
+      <div className={styles.BreedContentBox}>
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+        <DogBreed />
+      </div>
     </div>
   );
 }
