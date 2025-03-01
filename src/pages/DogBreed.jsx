30ce937a643516bda7494cce4d1c36ab38d1fa8f
Sahuan <sahuanpimentel@gmail.com>
2025-03-01 10:59:23 -0300

    Renamed js files to jsx files. Created styles and scoped styles for components.

--- src/pages/DogBreed.jsx
+++ src/pages/DogBreed.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./DogBreed.module.css";
 
 export default function DogBreed() {
   return <div>DogBreed</div>;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.header}>
+      <h1>Teste de funcionalidade da página Home!</h1>
+    </div>
+  );
+}
